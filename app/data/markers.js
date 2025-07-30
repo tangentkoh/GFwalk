83c5d308ba067bea6b47c6ccbe9741b8e5bde6f2
tangentkoh <k.hori.672@stn.nitech.ac.jp>
2025-07-30 18:09:05 +0900

    dkymu

--- app/data/markers.js
+++ app/data/markers.js
@@ -9,10 +9,24 @@ const markers = [
     image: "/image/岐阜城.png",
   },
   {
+    id: "gifu_castle_rakuichi",
+    position: [35.435, 136.773],
+    name: "岐阜城楽市",
+    description: "金華山の下に、地元の特産品を楽しむ場",
+    image: "/image/岐阜城楽市.png",
+  },
+  {
+    id: "kinpouzan_shoufukuji",
+    position: [35.432, 136.772],
+    name: "金鳳山正法寺",
+    description: "日本三大仏の一つ、観る意味はきっと在る",
+    image: "/image/金鳳山正法寺.png",
+  },
+  {
     id: "gifu_park",
     position: [35.435, 136.774],
     name: "岐阜公園",
-    description: "金華山の麓に広がる歴史ある公園",
+    description: "金華山の麓に広がる、歴史ある公園",
     image: "/image/岐阜公園.png",
   },
   {
@@ -26,7 +40,7 @@ const markers = [
     id: "gifu_history_museum",
     position: [35.433, 136.773],
     name: "岐阜市歴史博物館",
-    description: "岐阜の歴史と文化を学べる博物館",
+    description: "岐阜の歴史と文化を学ぶ、博物館",
     image: "/image/岐阜市歴史博物館.png",
   },
   {
