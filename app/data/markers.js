2688c9d4f3d9fdfaed656ef9c61b4c5cd01ee5ef
tangentkoh <k.hori.672@stn.nitech.ac.jp>
2025-07-30 18:29:26 +0900

    45wyh

--- app/data/markers.js
+++ app/data/markers.js
@@ -30,11 +30,32 @@ const markers = [
     image: "/image/岐阜公園.png",
   },
   {
+    id: "gifu_media_cosmos",
+    position: [35.427, 136.76],
+    name: "ぎふメディアコスモス",
+    description: "知の拠点の役割を担う、図書館と文化施設",
+    image: "/image/ぎふメディアコスモス.png",
+  },
+  {
+    id: "nagaragawa_ukai_museum",
+    position: [35.441, 136.779],
+    name: "長良川鵜飼ミュージアム",
+    description: "長良川の畔で、鵜飼について知る",
+    image: "/image/長良川鵜飼ミュージアム.png",
+  },
+  {
     id: "jr_gifu_station",
     position: [35.41, 136.757],
     name: "JR岐阜駅",
     description: "岐阜市の主要駅、金の織田信長が居る",
     image: "/image/JR岐阜駅.png",
+  },
+  {
+    id: "yanagase_shopping_street",
+    position: [35.419, 136.759],
+    name: "柳ケ瀬商店街",
+    description: "純粋な商店街、嘗ての賑やかな市場は今どこに",
+    image: "/image/柳ケ瀬商店街.png",
   },
   {
     id: "gifu_history_museum",
